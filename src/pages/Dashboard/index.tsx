94dccfbd228b6c22e11a99193e356ecc2f7e459c
Mateus Bezerra <mateusbzerra@gmail.com>
2020-04-23 20:43:59 -0300

    style: styled-components & react-router-dom setup

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const Dashboard: React.FC = () => (
+  <Container>
+    <Title>Explore repositórios no Github</Title>
+  </Container>
+);
+
+export default Dashboard;
